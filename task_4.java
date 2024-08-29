f3aa2f322f62779b9fec24abce85dd2f8f847698
BoHdANchIck <daneliuk.bohdan@chnu.edu.ua>
2024-08-29 08:25:00 +0300

    new commit

--- task_4.java
+++ task_4.java
@@ -3,11 +3,47 @@ import java.util.Scanner;
 public class task_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // String text = scanner.nextLine();
-        // String symbol = scanner.nextLine();
-        // for (int i = 0; i < text.length(); i++) {
-        // System.out.println(text);
-        // }
+
+        System.out.print("Enter the symbol: ");
+        char searchChar = scanner.next().charAt(0);
+        scanner.nextLine();
+
+        System.out.print("Enter the text: ");
+        String text = scanner.nextLine();
+
+        String[] words = text.split("[\\s,!.?;:]+");
+
+        int maxFrequency = 0;
+        String[] resultWords = new String[words.length];
+        int resultIndex = 0;
+
+        for (String word : words) {
+            int frequency = countCharFrequency(word, searchChar);
+
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+                resultIndex = 0;
+                resultWords[resultIndex++] = word;
+            } else if (frequency == maxFrequency) {
+                resultWords[resultIndex++] = word;
+            }
+        }
+
+        System.out.println("Words with the maximum frequency of '" + searchChar + "':");
+        for (int i = 0; i < resultIndex; i++) {
+            System.out.println(resultWords[i]);
+        }
+
         scanner.close();
+    }
+
+    private static int countCharFrequency(String word, char searchChar) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (c == searchChar) {
+                count++;
+            }
+        }
+        return count;
     }
 }
